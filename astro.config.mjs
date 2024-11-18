c4e4cd11ff59016da8b635dbd87eb63742c7c8fc
Anthony O'Connell <tony@one.ie>
2024-11-18 17:28:41 +0700

    Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,11 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    maxDuration: 8
+  }),
   integrations: [react(), tailwind()]
 });
